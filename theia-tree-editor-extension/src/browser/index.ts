1a5e673cb9f4fa36971db9c099812429334e43af
Edgar Mueller <emueller@eclipsesource.com>
2018-08-09 12:34:07 +0200

    Move common utils used by clients into extension

--- theia-tree-editor-extension/src/browser/index.ts
+++ theia-tree-editor-extension/src/browser/index.ts
@@ -1,1 +1,25 @@
+import { getSchema, getUiSchema } from '@jsonforms/core';
 export * from './theia-tree-editor-open-handler';
+
+
+export interface TreeEditorAppProps {
+  uischema: any;
+  schema: any;
+  filterPredicate: any;
+  labelProvider: any;
+  imageProvider: any;
+  saveable: any;
+}
+
+
+export const mapStateToTreeEditorAppProps = (state, ownProps) => {
+  return {
+    uischema: getUiSchema(state),
+    schema: getSchema(state),
+    filterPredicate: ownProps.filterPredicate,
+    labelProvider: ownProps.labelProvider,
+    imageProvider: ownProps.imageProvider
+  };
+};
+
+export * from './tree-editor-utils'
